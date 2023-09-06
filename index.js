0acfb94980528d9dfc706c3f179482b1e7e1e135
Ni.Bagherpour <ni.bagherpour@sadad.co.ir>
2023-09-06 14:43:57 +0330

    feat: add cake feature

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('From index');
+const store = require('./app/store')
+const cakeActions = require('./features/cake/cake-slice').cakeActions
+
+console.log('Initial State ', store.getState())
+const unsubscribe = store.subscribe(() => {
+  console.log('Updated State ', store.getState())
+})
+
+store.dispatch(cakeActions.ordered())
+store.dispatch(cakeActions.ordered())
+store.dispatch(cakeActions.ordered())
+store.dispatch(cakeActions.restocked(3))
+
+
+unsubscribe()

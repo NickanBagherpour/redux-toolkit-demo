7e9052e66975e95dde51774bd6d37d833edf7531
Ni.Bagherpour <ni.bagherpour@sadad.co.ir>
2023-09-07 10:42:37 +0330

    feat: add icecream feature

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const store = require('./app/store')
 const cakeActions = require('./features/cake/cake-slice').cakeActions
+const icecreamActions = require('./features/icecream/icecream-slice').icecreamActions
 
 console.log('Initial State ', store.getState())
 const unsubscribe = store.subscribe(() => {
@@ -10,6 +11,9 @@ store.dispatch(cakeActions.ordered())
 store.dispatch(cakeActions.ordered())
 store.dispatch(cakeActions.ordered())
 store.dispatch(cakeActions.restocked(3))
-
+store.dispatch(icecreamActions.ordered())
+store.dispatch(icecreamActions.ordered())
+store.dispatch(icecreamActions.ordered())
+store.dispatch(icecreamActions.restocked(3))
 
 unsubscribe()
